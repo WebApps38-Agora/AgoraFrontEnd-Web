3f35f2a765811956b7212a3be2c7697021bac37a
Chris Hawkes <chawkesmexico@gmail.com>
2017-06-10 18:47:54 +0100

    Fetch from heroku back end.

--- src/Comments.js
+++ src/Comments.js
@@ -7,17 +7,30 @@ class Comments extends Component {
     this.state = {comments: [1,2,3,4]};
   }
 
+  componentDidMount() {
+    this.loadCommentsFromServer();
+  }
+
+  loadCommentsFromServer = () => {
+    const component = this;
+    fetch('https://agora-be.herokuapp.com/').then(function(response) {
+      return response.json();
+    }).then(function(j) {
+      component.setState({comments: [j.articles]});
+    });
+  }
+
   render() {
-    const cs = this.state.comments.map((comment) =>
-        <li> {comment} </li>
+    const cs = this.state.comments.map((comment, index) =>
+        <li key={index}> {comment} </li>
     );
 
     return (
-      <div id="center">
-      <div id="inner">
-        <ul>{cs}</ul>
+      <div className="center">
+        <div className="inner">
+          <ul>{cs}</ul>
+        </div>
       </div>
-    </div>
     );
   }
 }
