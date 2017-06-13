5f981e89171dc153feba3a2678c71f6a30aea430
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-13 22:49:20 +0100

    No longer using modal for adding facts

--- src/containers/FactSection.js
+++ src/containers/FactSection.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Divider, Button, Form, Segment } from 'semantic-ui-react'
 
 import Facts from '../presentational/Facts';
 import { fetchFacts } from '../actions/FactSection';
+import '../style/FactSection.css'
 
 class FactSection extends Component {
   render() {
-    return !this.props.isFetching && <Facts facts={this.props.topic.fact_set} />
+    return !this.props.isFetching &&
+    <div id="fact-section">
+      <Segment vertical id="facts">
+      <Facts facts={this.props.topic.fact_set}/>
+      </Segment>
+      <Segment vertical>
+        <Form success={false}>
+          <Form.Group>
+            <Form.Input width={12} placeholder='Write an unbiased, objective fact about this news topic' />
+            <Button width={4} content='Add Fact' labelPosition='left' icon='edit' primary />
+          </Form.Group>
+        </Form>
+      </Segment>
+    </div>
   }
 }
 
--- src/presentational/Facts.js
+++ src/presentational/Facts.js
@@ -10,15 +10,6 @@ export default class Facts extends Component {
     return (
       <div>
         {facts}
-
-        <Modal dimmer={false} trigger={
-            <Button content='Add Fact' labelPosition='left' icon='edit' primary/>
-          }>
-          <Modal.Header>Add a fact</Modal.Header>
-          <Modal.Content>
-            hi
-          </Modal.Content>
-        </Modal>
       </div>
     );
   }
