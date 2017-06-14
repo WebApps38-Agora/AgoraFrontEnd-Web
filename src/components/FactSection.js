b34d4737273970c03a16c40a45245a5c5082c324
Rafael Ballestiero <rb2215@ic.ac.uk>
2017-06-15 00:49:03 +0100

    Fixed code related to FactSection

--- src/components/FactSection.js
+++ src/components/FactSection.js
@@ -23,49 +23,47 @@ class FactSection extends Component {
   }
 
   render() {
-    if (this.props) {
-      let facts = this.props.topic.fact_set.map((fact, index) =>
-          <Card key={index} header={fact.content} fluid />
-      );
+    let facts = this.props.topic.fact_set.map((fact, index) =>
+        <Card key={index} header={fact.content} fluid />
+    );
 
-      if (!facts.length) {
-        facts = (<div className="missing">
-                  <div className="missing-inner">
-                    <Icon name="inbox" size="massive" />
-                    <h1>No facts on this topic!</h1>
-                    <p>Be the first person to add an unbiased fact to this topic.</p>
-                  </div>
-                 </div>);
-      }
+    if (!facts.length) {
+      facts = (<div className="missing">
+                <div className="missing-inner">
+                  <Icon name="inbox" size="massive" />
+                  <h1>No facts on this topic!</h1>
+                  <p>Be the first person to add an unbiased fact to this topic.</p>
+                </div>
+               </div>);
+    }
 
-      return (
-        <div className="section" id="fact-section">
-          <Segment vertical className="section-content" id="facts">
-            {facts}
-          </Segment>
-          <Segment vertical>
-            <Form success={false}>
-              <Form.Group className="section-form">
-                <Form.Input style={{width: 100 + "%"}}
-                            id="fact-input"
-                            value={this.state.fact_content}
-                            onChange={(e, {name, value}) => this.setState({fact_content: value})}
-                            placeholder='Write an unbiased, objective fact about this news topic...' />
+    return (
+      <div className="section" id="fact-section">
+        <Segment vertical className="section-content" id="facts">
+          {facts}
+        </Segment>
+        <Segment vertical>
+          <Form success={false}>
+            <Form.Group className="section-form">
+              <Form.Input style={{width: 100 + "%"}}
+                          id="fact-input"
+                          value={this.state.fact_content}
+                          onChange={(e, {name, value}) => this.setState({fact_content: value})}
+                          placeholder='Write an unbiased, objective fact about this news topic...' />
 
-                <Button id="fact-button"
-                        onClick={this.handleSubmit}
-                        disabled={this.checkInputEmpty()}
-                        content='Add Fact'
-                        labelPosition='left'
-                        icon='edit'
-                        primary />
-              </Form.Group>
-            </Form>
-          </Segment>
-        </div>
-      )
+              <Button id="fact-button"
+                      onClick={this.handleSubmit}
+                      disabled={this.checkInputEmpty()}
+                      content='Add Fact'
+                      labelPosition='left'
+                      icon='edit'
+                      primary />
+            </Form.Group>
+          </Form>
+        </Segment>
+      </div>
+    )
   }
 }
 
-export default connect(
-)(FactSection)
+export default connect()(FactSection)
