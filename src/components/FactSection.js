50253206fecd3dc87e82b81b44632f8a1614f561
Rafael Ballestiero <rb2215@ic.ac.uk>
2017-06-14 15:37:20 +0100

    Added responsiveness to facts section

--- src/components/FactSection.js
+++ src/components/FactSection.js
@@ -37,15 +37,16 @@ class FactSection extends Component {
           </Segment>
           <Segment vertical>
             <Form success={false}>
-              <Form.Group>
-                <Form.Input value={this.state.fact_content}
+              <Form.Group id="fact-form">
+                <Form.Input style={{width: 100 + "%"}}
+                            id="fact-input"
+                            value={this.state.fact_content}
                             onChange={(e, {name, value}) => this.setState({fact_content: value})}
-                            width={12}
                             placeholder='Write an unbiased, objective fact about this news topic' />
 
-                <Button onClick={this.handleSubmit}
+                <Button id="fact-button"
+                        onClick={this.handleSubmit}
                         disabled={this.checkInputEmpty()}
-                        width={4}
                         content='Add Fact'
                         labelPosition='left'
                         icon='edit'
