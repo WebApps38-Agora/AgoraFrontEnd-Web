dea37455890408f31ece873b53e31eaca6cbe116
Norbert Podsadowski <norbo11@googlemail.com>
2017-06-18 22:07:15 +0100

    Added political stance from backend into comments

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -67,7 +67,7 @@ class CommentSection extends Component {
         <Comment.Content>
           <Comment.Author as='a' href='/profile/'>{profile.first_name} {profile.last_name}</Comment.Author>
           <Comment.Metadata>
-            <Label color='green'>Left-wing Liberal</Label>
+            <Label color={profile.political_color}>{profile.political_stance}</Label>
           </Comment.Metadata>
           <Comment.Text>
             <p>{comment.content}</p>
